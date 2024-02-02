1bc4ab566954d15b3b56d35792b03e0fed1c91a0
labib3013 <alfarabyhd30@gmail.com>
2024-02-03 01:21:31 +0700

    feat: navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/login.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class Post {
-  String body;
-  String author;
-  int likes = 0;
-  bool userLiked = false;
-
-  Post(this.body, this.author);
-
-  void likePost() {
-    this.userLiked = !this.userLiked;
-    if (this.userLiked) {
-      likes += 1;
-    } else {
-      likes -= 1;
-    }
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -32,129 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Post> posts = [];
-  void changeText(String text) {
-    this.setState(() {});
-  }
-
-  void newPost(String text) {
-    this.setState(() {
-      posts.add(new Post(text, "Author"));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello World"),
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(child: PostList(this.posts)),
-          TextInputWidget(this.newPost),
-        ],
-      ),
-    );
-  }
-}
-
-class TextInputWidget extends StatefulWidget {
-  final Function(String) callback;
-
-  const TextInputWidget(this.callback);
-
-  @override
-  _TextInputWidgetState createState() => _TextInputWidgetState();
-}
-
-class _TextInputWidgetState extends State<TextInputWidget> {
-  final controller = TextEditingController();
-
-  @override
-  void dispose() {
-    super.dispose();
-    controller.dispose();
-  }
-
-  void click() {
-    widget.callback(controller.text);
-    controller.clear();
-    FocusScope.of(context).unfocus();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      controller: this.controller,
-      decoration: InputDecoration(
-          prefixIcon: Icon(Icons.message),
-          labelText: "Type a message:",
-          suffixIcon: IconButton(
-            icon: Icon(Icons.send),
-            onPressed: this.click,
-            splashColor: Colors.blue,
-            tooltip: "Post message",
-          )),
-    );
-  }
-}
-
-class PostList extends StatefulWidget {
-  final List<Post> listItems;
-
-  PostList(this.listItems);
-
-  @override
-  State<PostList> createState() => _PostListState();
-}
-
-class _PostListState extends State<PostList> {
-  void like(Function callback) {
-    this.setState(() {
-      callback();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: this.widget.listItems.length,
-      itemBuilder: (context, index) {
-        var post = this.widget.listItems[index];
-
-        return Card(
-            child: Row(children: <Widget>[
-          Expanded(
-              child: ListTile(
-                  title: Text(post.body), subtitle: Text(post.author))),
-          Row(children: <Widget>[
-            Container(
-              child: Text(
-                post.likes.toString(),
-                style: TextStyle(fontSize: 20),
-              ),
-              padding: EdgeInsets.all(20),
-            ),
-            IconButton(
-              onPressed: () => this.like(post.likePost),
-              icon: Icon(Icons.thumb_up),
-              color: post.userLiked ? Colors.blue : Colors.grey,
-            )
-          ])
-        ]));
-      },
+      home: LoginPage(),
     );
   }
 }
